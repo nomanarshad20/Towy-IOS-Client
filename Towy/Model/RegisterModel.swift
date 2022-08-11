4529879899bf3bb6003de75abf7d4e3cb7edaf91
Danish Irshad <danish.irshad@upgenicsint.work>
2022-08-11 16:35:50 +0500

    register user model updated

--- Towy/Model/RegisterModel.swift
+++ Towy/Model/RegisterModel.swift
@@ -6,39 +6,56 @@
 //
 
 import Foundation
+import SwiftyJSON
 
 
 // MARK: - RegisterUser
 struct RegisterUser: Codable {
     let result, message: String
-    let data: RegiaterDataClass
+    let data: RegisterDataClass
 }
 
 // MARK: - DataClass
-struct RegiaterDataClass: Codable {
-    let userID: Int
-    let email, mobileNo, fcmToken, userType: String
-    let isVerified: Int
-    let referralCode: String
-    let steps: Int
-    let provider, image: String?
-    let firstName, lastName: String
-    let walletBalance: Int
-    let accessToken: String
-
-    enum CodingKeys: String, CodingKey {
-        case userID = "user_id"
-        case email
-        case mobileNo = "mobile_no"
-        case fcmToken = "fcm_token"
-        case userType = "user_type"
-        case isVerified = "is_verified"
-        case referralCode = "referral_code"
-        case steps, provider, image
-        case firstName = "first_name"
-        case lastName = "last_name"
-        case walletBalance = "wallet_balance"
-        case accessToken = "access_token"
+struct RegisterDataClass: Codable {
+    let userID: String!
+    let email: String!
+    let mobileNo: String!
+    let fcmToken: String!
+    let userType:Int!
+    let isVerified: Int!
+    let referralCode: String!
+    let firstName, lastName: String!
+    let walletBalance: Int!
+    let accessToken: String!
+    
+    init(fromJson json: JSON!){
+        userID = json["user_id"].string
+        email = json["email"].string
+        mobileNo = json["mobile_no"].string
+        fcmToken = json["fcm_token"].string
+        userType = json["user_type"].int
+        isVerified = json["is_verified"].int
+        referralCode = json["referral_code"].string
+        firstName = json["first_name"].string
+        lastName = json["last_name"].string
+        walletBalance = json["wallet_balance"].int
+        accessToken = json["access_token"].string
+    }
+    
+    init(coder aDecoder: NSCoder)
+    {
+        userID = aDecoder.decodeObject(forKey: "user_id") as? String
+        email = aDecoder.decodeObject(forKey: "email") as? String
+        mobileNo = aDecoder.decodeObject(forKey: "mobile_no") as? String
+        fcmToken = aDecoder.decodeObject(forKey: "fcm_token") as? String
+        userType = aDecoder.decodeObject(forKey: "user_type") as? Int
+        isVerified = aDecoder.decodeObject(forKey: "is_verified") as? Int
+        referralCode = aDecoder.decodeObject(forKey: "referral_code") as? String
+        firstName = aDecoder.decodeObject(forKey: "first_name") as? String
+        lastName = aDecoder.decodeObject(forKey: "last_name") as? String
+        walletBalance = aDecoder.decodeObject(forKey: "wallet_balance") as? Int
+        accessToken = aDecoder.decodeObject(forKey: "access_token") as? String
     }
 }
+
 
